29d65429f291a9f27028c1e104c44015e074f548
aline <linegcruz@gmail.com>
2023-01-02 20:29:12 -0300

    Added: doctors register.

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -1,4 +1,4 @@
-package med.voll.api;
+package med.voll.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String olaMundo(){
+    public String olaMundo() {
         return "Hello World com Spring! Yeah!";
     }
 
---
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,12 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEnderecos;
+
+public record DadosCadastroMedico(
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade,
+        DadosEnderecos endereco
+) {
+}
