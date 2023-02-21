1f5d92b032fb4c537bb33628625e81851c407f80
aline <linegcruz@gmail.com>
2023-02-21 13:14:01 -0300

    Added: validation in doctor registration with bean validation.

--- src/main/java/med/voll/api/endereco/DadosEnderecos.java
+++ src/main/java/med/voll/api/endereco/DadosEnderecos.java
@@ -1,10 +1,20 @@
 package med.voll.api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosEnderecos(
+
+        @NotBlank
         String logradouro,
+        @NotNull
         String bairro,
+        @NotNull @Pattern(regexp = "\\d{8}")
         String cep,
+        @NotNull
         String cidade,
+        @NotNull
         String uf,
         String complemento,
         String numero
